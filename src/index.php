367a3ebbf837f4ccfccb6f1c1c5535b07fb0bc2d
Rene B. Pinto <renebentes@yahoo.com.br>
2025-09-19 11:33:51 -0300

    feat: adicione os arquivos iniciais do tema
    A estrutura inicial do tema GOV.BR para Joomla 5, inclui arquivos de template, diretórios e arquivos
    de idioma. Por se tratar de um tema para o público brasileiro, os arquivos de idioma estão
    configurados para pt-BR e en-GB, sendo o último para garantir compatibilidade com o padrão do Joomla.

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.GovBRDS
+ *
+ * @author      Rene Bentes Pinto <renebentes@yahoo.com.br>
+ * @copyright   Copyright (C) 2025 Rene Bentes Pinto. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * @link        http://renebentes.github.io
+ * @since       __DEPLOY_VERSION__
+ */
+
+// No direct access.
+defined('_JEXEC') or die('Restricted access!');
+
+echo '<h1>Hello World</h1>';
