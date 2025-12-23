6f15e0f0eb398acab7015312fb9eb541c63cc70d
Rene B. Pinto <renebentes@yahoo.com.br>
2025-12-23 08:19:47 -0300

    feat(devcontainer): aprimore script de criação
    O script de criação foi aprimorado com mensagens de progresso, alteração no tempo de espera da
    checagem de serviços e melhorias gerais na legibilidade do código. Além disso, alterou-se a forma
    como o `Composer` lida com as dependências, e, corrigiu-se as permissões de diretórios para que
    não haja mais problemas de acesso quando usamos o gerenciador do `Joomla`.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -9,24 +9,35 @@ JOOMLA_CLI=${WEB_DIR}/cli/joomla.php
 
 sudo chmod a+x "${BASE_DIR}"
 
-composer update
+echo "--> Installing Composer and NPM dependencies..."
+composer install
 npm install
-npm run build
+echo "✅ Dependencies installed."
 
-# Wait for the web server to be available
+echo "--> Waiting for the web server to be available"
 while [ $(curl -sk "http://localhost:8080" -o /dev/null -w "%{http_code}") -ne 200 ]; do
-    sleep 5
+    sleep 1
 done
+echo "✅ Web server is available."
 
+echo "--> Building the template and running Phing build tasks..."
+npm run build
 sudo vendor/bin/phing
+echo "✅ Build completed."
 
-sudo chown -R ${USERNAME}:www-data ${WEB_DIR}
-
+echo "--> Installing and enabling the GovBR template..."
 ${JOOMLA_CLI} extension:discover
 ${JOOMLA_CLI} extension:discover:install
+echo "✅ GovBR template installed."
 
+echo "--> Applying development settings..."
 sudo su -c "${JOOMLA_CLI} config:set debug=true"
 sudo su -c "${JOOMLA_CLI} config:set error_reporting=maximum"
+echo "✅ Development settings applied."
+
+echo "--> Setting group ownership and permissions..."
+sudo chown -R ${USERNAME}:www-data ${WEB_DIR}
+sudo chmod -R g+rws ${WEB_DIR}
 
 echo "Please, go to http://localhost:8080/administrator/index.php?option=com_templates&view=styles to enable the template as default."
-echo "Done!"
+echo "✅ Done!"
