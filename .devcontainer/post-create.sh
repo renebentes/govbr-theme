c7263228d71cc42c2bfa912ac07e350ea65a22b6
Rene B. Pinto <renebentes@yahoo.com.br>
2026-05-08 11:43:14 -0300

    chore(devcontainer): altere a forma de instalação do template

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,39 +4,68 @@ set -e
 USERNAME="vscode"
 BASE_DIR=$(pwd)/govbr
 WEB_DIR=/var/www/html
-TEMPLATE_PATH=${WEB_DIR}/templates/govbr
-JOOMLA_CLI=${WEB_DIR}/cli/joomla.php
+TEMPLATE_PATH="${WEB_DIR}/templates/govbr"
+JOOMLA_CLI="${WEB_DIR}/cli/joomla.php"
 
-sudo chmod a+x "${BASE_DIR}"
+echo "--> Waiting for MariaDB server to be available..."
+while ! mariadb-admin ping -h"db" --skip-ssl -u root -p"$JOOMLA_DB_PASSWORD" --silent; do
+    sleep 1
+done
+echo "✅ MariaDB server is ready."
 
 echo "--> Installing Composer and NPM dependencies..."
 composer install
 npm install
 echo "✅ Dependencies installed."
 
-echo "--> Waiting for the web server to be available"
-while [ $(curl -sk "http://localhost:8080" -o /dev/null -w "%{http_code}") -ne 200 ]; do
-    sleep 1
-done
-echo "✅ Web server is available."
+echo "--> Installing Joomla using the official package downloaded..."
+sudo php "${WEB_DIR}/installation/joomla.php" install \
+    --site-name="${JOOMLA_SITE_NAME}" \
+    --admin-user="${JOOMLA_ADMIN_USER}" \
+    --admin-username="${JOOMLA_ADMIN_USERNAME}" \
+    --admin-password="${JOOMLA_ADMIN_PASSWORD}" \
+    --admin-email="${JOOMLA_ADMIN_EMAIL}" \
+    --db-type="mysqli" \
+    --db-host="db" \
+    --db-name="${JOOMLA_DB_NAME}" \
+    --db-user="${JOOMLA_DB_USER}" \
+    --db-pass="${JOOMLA_DB_PASSWORD}" \
+    --db-prefix="gov_" \
+    --db-encryption="0" \
+    --public-folder=""
+echo "✅ Joomla installed."
 
-echo "--> Building the template and running Phing build tasks..."
+echo "--> Building the template..."
 npm run build
-vendor/bin/phing
 echo "✅ Build completed."
 
-echo "--> Installing and enabling the GovBR template..."
-sudo su -c "${JOOMLA_CLI} extension:install --path=./build/govbr.tar.gz"
+echo "--> Adding symbolic links for files and directories..."
+sudo ln -snf "${BASE_DIR}" "${WEB_DIR}/templates/govbr"
+sudo ln -snf "${BASE_DIR}/media" "${WEB_DIR}/media/templates/site/govbr"
+
+for relative_path in $(find "${BASE_DIR}/language/" -type f); do
+    clean_path="${relative_path#${BASE_DIR}/language/}"
+    lang_code=$(dirname "${clean_path}")
+    sudo mkdir -p "${WEB_DIR}/language/${lang_code}"
+    sudo ln -sf "${BASE_DIR}/language/${clean_path}" "$WEB_DIR/language/${clean_path}"
+done
+echo "✅ Symbolic links added."
+
+echo "--> Installing the GovBR template..."
+sudo php "${JOOMLA_CLI}" extension:discover
+sudo php "${JOOMLA_CLI}" extension:discover:install
 echo "✅ GovBR template installed."
 
 echo "--> Applying development settings..."
-sudo su -c "${JOOMLA_CLI} config:set debug=true"
-sudo su -c "${JOOMLA_CLI} config:set error_reporting=maximum"
+sudo php "${JOOMLA_CLI}" config:set debug=true
+sudo php "${JOOMLA_CLI}" config:set error_reporting=maximum
 echo "✅ Development settings applied."
 
 echo "--> Setting group ownership and permissions..."
-sudo chown -R ${USERNAME}:www-data ${WEB_DIR}
-sudo chmod -R g+rws ${WEB_DIR}
+sudo chmod a+x "${BASE_DIR}"
+sudo chown -R "${USERNAME}":www-data "${WEB_DIR}"
+sudo chmod -R g+rws "${WEB_DIR}"
+echo "✅ Permissions applied."
 
 echo "Please, go to http://localhost:8080/administrator/index.php?option=com_templates&view=styles to enable the template as default."
 echo "✅ Done!"
