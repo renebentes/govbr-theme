714cbb36a49f53ace5affd993f06b5978c4a38d8
Rene B. Pinto <renebentes@yahoo.com.br>
2026-05-25 14:28:35 -0300

    feat(info): atualize como detalhes do artigo são exibidos
    Os detalhes dos artigos, categoria, datas de criação e afins, foram separados para que apenas
    informações de atualização fossem exibidos no topo do artigo. Nome de categoria, associações e
    contador de acessos são exibidos no rodapé do artigo

--- govbr/html/layouts/joomla/content/info_block.php
+++ govbr/html/layouts/joomla/content/info_block.php
@@ -32,6 +32,23 @@ $blockPosition = $displayData['params']->get('info_block_position', 0);
             <?php echo $this->sublayout('author', $displayData); ?>
         <?php endif; ?>
 
+        <?php if ($displayData['params']->get('show_create_date')) : ?>
+            <?php echo $this->sublayout('create_date', $displayData); ?>
+        <?php endif; ?>
+
+        <?php if ($displayData['params']->get('show_modify_date')) : ?>
+            <?php echo $this->sublayout('modify_date', $displayData); ?>
+        <?php endif; ?>
+
+        <?php if ($displayData['params']->get('show_publish_date')) : ?>
+            <?php echo $this->sublayout('publish_date', $displayData); ?>
+        <?php endif; ?>
+    <?php endif; ?>
+
+    <?php if (
+        ($displayData['position'] === 'below'
+    && ($blockPosition == 1 || $blockPosition == 2))
+    ) : ?>
         <?php if ($displayData['params']->get('show_parent_category') && !empty($displayData['item']->parent_id)) : ?>
             <?php echo $this->sublayout('parent_category', $displayData); ?>
         <?php endif; ?>
@@ -42,23 +59,6 @@ $blockPosition = $displayData['params']->get('info_block_position', 0);
 
         <?php if ($displayData['params']->get('show_associations')) : ?>
             <?php echo $this->sublayout('associations', $displayData); ?>
-        <?php endif; ?>
-
-        <?php if ($displayData['params']->get('show_publish_date')) : ?>
-            <?php echo $this->sublayout('publish_date', $displayData); ?>
-        <?php endif; ?>
-    <?php endif; ?>
-
-    <?php if (
-        ($displayData['position'] === 'above' && ($blockPosition == 0))
-        || ($displayData['position'] === 'below' && ($blockPosition == 1 || $blockPosition == 2))
-    ) : ?>
-        <?php if ($displayData['params']->get('show_create_date')) : ?>
-            <?php echo $this->sublayout('create_date', $displayData); ?>
-        <?php endif; ?>
-
-        <?php if ($displayData['params']->get('show_modify_date')) : ?>
-            <?php echo $this->sublayout('modify_date', $displayData); ?>
         <?php endif; ?>
 
         <?php if ($displayData['params']->get('show_hits')) : ?>
