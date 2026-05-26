3dd8c5b40efb8925c19f711156bcc406a6eef726
Rene B. Pinto <renebentes@yahoo.com.br>
2026-05-26 16:45:06 -0300

    feat(TOC): introduz layout para exibição de tabela de conteúdo
    Este layout está em desenvolvimento, necessitando de mais polimento para ser usado

---
+++ govbr/html/plg_content_pagebreak/navigation.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.GovBR
+ *
+ * @author      Rene Bentes Pinto <renebentes@yahoo.com.br>
+ * @copyright   Copyright (C) 2026 Rene Bentes Pinto. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * @since       __DEPLOY_VERSION__
+ */
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+use Joomla\Plugin\Content\PageBreak\Extension\PageBreak;
+
+/**
+ * @var PageBreak  $this
+ * @var array      $links  Array with keys 'previous' and 'next' with non-SEO links to the previous and next pages
+ * @var integer    $page   The page number
+ */
+
+$lang = $this->getApplication()->getLanguage();
+
+?>
+<nav class="br-pagination mt-4">
+    <div class="pagination-arrows">
+        <?php if ($links['previous']) :
+            $direction = $lang->isRtl() ? 'right' : 'left';
+            $title     = htmlspecialchars($this->list[$page]->title, ENT_QUOTES, 'UTF-8');
+            $ariaLabel = Text::_('JPREVIOUS') . ': ' . $title . ' (' . Text::sprintf('JLIB_HTML_PAGE_CURRENT_OF_TOTAL', $page, $n) . ')';
+            ?>
+        <a class="br-button secondary"
+            href="<?php echo Route::_($links['previous']); ?>"
+            title="<?= $title; ?>"
+            aria-label="<?= $ariaLabel; ?>"
+            rel="prev">
+            <i class="fas fa-chevron-<?= $direction; ?>" aria-hidden="true"></i>
+            <?= Text::_('JPREV'); ?>
+        </a>
+        <?php endif; ?>
+        <?php if ($links['next']) :
+            $direction = $lang->isRtl() ? 'left' : 'right';
+            $title     = htmlspecialchars($this->list[$page + 2]->title, ENT_QUOTES, 'UTF-8');
+            $ariaLabel = Text::_('JNEXT') . ': ' . $title . ' (' . Text::sprintf('JLIB_HTML_PAGE_CURRENT_OF_TOTAL', $page + 2, $n) . ')';
+            ?>
+        <a class="br-button secondary"
+            href="<?php echo Route::_($links['next']); ?>"
+            title="<?= $title; ?>"
+            aria-label="<?= $ariaLabel; ?>"
+            rel="next">
+            <?= Text::_('JNEXT'); ?>
+            <i class="fas fa-chevron-<?= $direction; ?>" aria-hidden="true"></i>
+        </a>
+        <?php endif; ?>
+    </div>
+</nav>
