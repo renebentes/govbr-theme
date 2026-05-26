9391366374d74771c76aebb60c8dae72405f621e
Rene B. Pinto <renebentes@yahoo.com.br>
2026-05-26 16:45:06 -0300

    feat(TOC): introduz layout para exibição de tabela de conteúdo
    Este layout está em desenvolvimento, necessitando de mais polimento para ser usado

---
+++ govbr/html/plg_content_pagebreak/toc.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.GovBR
+ *
+ * @author      Rene Bentes Pinto <renebentes@yahoo.com.br>
+ * @copyright   Copyright (C) 2026 Rene Bentes Pinto. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ * @since       __DEPLOY_VERSION__
+ */
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+
+?>
+<div class="br-menu contextual" aria-labelledby="#toc">
+    <div class="menu-trigger">
+        <button type="button" class="br-button primary block" data-toggle="contextual">
+            <span id="toc" class="mr-1"><?= Text::_('PLG_CONTENT_PAGEBREAK_TOC_LABEL'); ?></span>
+            <i class="fas fa-chevron-up ml-5" aria-hidden="true"></i>
+        </button>
+    </div>
+    <div class="menu-container position-static">
+        <div class="menu-panel">
+            <nav class="menu-body">
+                <div class="menu-folder">
+                    <?php if ($headingtext) : ?>
+                        <div class="menu-item">
+                            <h4 class="content"><?= $headingtext; ?></h4>
+                        </div>
+
+                    <?php endif; ?>
+                    <ul>
+                        <?php foreach ($list as $listItem) : ?>
+                            <?php $class = $listItem->active ? ' active' : ''; ?>
+                            <li role="none">
+                                <a href="<?php echo Route::_($listItem->link); ?>" class="menu-item<?= $class; ?>">
+                                    <?= htmlspecialchars($listItem->title, ENT_QUOTES, 'UTF-8'); ?>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="menu-scrim" data-dismiss="menu" tabindex="0"></div>
+    </div>
+</div>
