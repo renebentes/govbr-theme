4048a6582f5157c30e0b74dcbf9a4f49292691fb
Rene B. Pinto <renebentes@yahoo.com.br>
2026-06-17 10:11:26 -0300

    style: padronize o cabeçalho dos arquivos

--- govbr/html/layouts/joomla/content/info_block/parent_category.php
+++ govbr/html/layouts/joomla/content/info_block/parent_category.php
@@ -5,7 +5,7 @@
  * @subpackage  Templates.GovBR
  *
  * @author      Rene Bentes Pinto <renebentes@yahoo.com.br>
- * @copyright   Copyright (C) 2026 Rene Bentes Pinto. All rights reserved.
+ * @copyright   Copyright (c) 2026 Rene Bentes Pinto. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  * @since       __DEPLOY_VERSION__
  */
